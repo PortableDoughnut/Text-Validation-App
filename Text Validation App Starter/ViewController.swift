9ef93a7fd3350ae6d8b980bf184c4b9699bbf440
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-02 08:22:46 -0600

    Inital Commit

--- Text Validation App Starter/ViewController.swift
+++ Text Validation App Starter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var statusMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func validatePassword(_ sender: Any) {
+        // TODO: Add Password validation logic here.
+    }
+    
 }
 
